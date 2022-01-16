70b19db7491f306a7ef7c7655a29c3605cfe82aa
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-16 17:37:38 -0500

    Template Strings

---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,15 @@
+const nombre = "Alejandro";
+const apellido = "Lizzt";
+
+// const nombreCompleto = nombre + " " + apellido;
+const nombreCompleto = `
+${nombre}
+${apellido}
+`;
+console.log(nombreCompleto);
+
+function getSaludo(nombre) {
+    return "Hola " + nombre;
+}
+
+console.log(`Este es un texto: ${getSaludo(nombre)} `);
