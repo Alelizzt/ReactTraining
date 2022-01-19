c452bcac9007d6fa88046d68fd0fa86e6d98435e
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-19 16:11:28 -0500

    utilizando Fetch api para traer las imagenes

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 const GifExpertApp = () => {
     // const categories = ["One Punch", "Danmachi", "Dragon Maid"];
-    const [categories, setCategories] = useState([
-        "One Punch",
-        "Danmachi",
-        "Dragon Maid",
-    ]);
+    const [categories, setCategories] = useState(["Dragon Maid"]);
 
     // const handleAdd = () => {
-    //     //setCategories(["YuGioH", ...categories]);
+    //setCategories(["YuGioH", ...categories]);
 
     // };
 
@@ -20,9 +17,9 @@ const GifExpertApp = () => {
             <AddCategory setCategories={setCategories} />
             <hr />
             <ol>
-                {categories.map((category) => {
-                    return <li key={category}>{category}</li>;
-                })}
+                {categories.map((category) => (
+                    <GifGrid key={category} category={category} />
+                ))}
             </ol>
         </>
     );
---
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const GifGrid = ({ category }) => {
+    const getGifs = async () => {
+        const apiKey = "h1749A5kBlb9kO8jerkATHG63RNkZvNz";
+        const serie = "Rick+and+Morty";
+        const resp = await fetch(
+            `https://api.giphy.com/v1/gifs/search?q=${serie}&limit=10&api_key=${apiKey}`
+        );
+        const { data } = await resp.json();
+        const gifs = data.map((img) => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images.downsized_medium.url,
+            };
+        });
+        console.log(gifs);
+    };
+
+    getGifs();
+    return (
+        <div>
+            <h3> {category}</h3>
+        </div>
+    );
+};
