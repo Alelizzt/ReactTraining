c08ac16ec7189fcafc52c04e0ce4bfa1bf2d59dc
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-19 15:49:10 -0500

    Comunicacion entre componentes y Proptype de funcion

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -2,22 +2,22 @@ import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 
 const GifExpertApp = () => {
-    const categories = ["One Punch", "Danmachi", "Dragon Maid"];
-    // const [categories, setCategories] = useState([
-    //     "One Punch",
-    //     "Danmachi",
-    //     "Dragon Maid",
-    // ]);
+    // const categories = ["One Punch", "Danmachi", "Dragon Maid"];
+    const [categories, setCategories] = useState([
+        "One Punch",
+        "Danmachi",
+        "Dragon Maid",
+    ]);
 
     // const handleAdd = () => {
     //     //setCategories(["YuGioH", ...categories]);
-    //     setCategories((cats) => [...categories, "YuGioH"]);
+
     // };
 
     return (
         <>
             <h2>GifExpertApp</h2>
-            <AddCategory />
+            <AddCategory setCategories={setCategories} />
             <hr />
             <ol>
                 {categories.map((category) => {
