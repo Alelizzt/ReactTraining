706803b723233faab7a69cc83189d245a65d3273
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-19 15:33:02 -0500

    Agregando componente AddCategory

--- 04-gif-expert-app/src/GifExpertApp.js
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -1,23 +1,24 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 const GifExpertApp = () => {
-    // const categories = ["One Punch", "Danmachi", "Dragon Maid"];
-    const [categories, setCategories] = useState([
-        "One Punch",
-        "Danmachi",
-        "Dragon Maid",
-    ]);
+    const categories = ["One Punch", "Danmachi", "Dragon Maid"];
+    // const [categories, setCategories] = useState([
+    //     "One Punch",
+    //     "Danmachi",
+    //     "Dragon Maid",
+    // ]);
 
-    const handleAdd = () => {
-        //setCategories(["YuGioH", ...categories]);
-        setCategories((cats) => [...categories, "YuGioH"]);
-    };
+    // const handleAdd = () => {
+    //     //setCategories(["YuGioH", ...categories]);
+    //     setCategories((cats) => [...categories, "YuGioH"]);
+    // };
 
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr />
-            <button onClick={handleAdd}>Agregar</button>
             <ol>
                 {categories.map((category) => {
                     return <li key={category}>{category}</li>;
