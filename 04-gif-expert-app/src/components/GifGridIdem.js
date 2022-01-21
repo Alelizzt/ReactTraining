87545039af79c449e4e723f348ca8cad6bfdb362
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-21 17:11:09 -0500

    Test de componentes

--- 04-gif-expert-app/src/components/GifGridIdem.js
+++ 04-gif-expert-app/src/components/GifGridIdem.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export const GifGridIdem = ({ id, title, url }) => {
+export const GifGridIdem = ({ title, url }) => {
     return (
         <div className="card animate__animated animate__fadeIn">
             <img src={url} alt={title} />
             <p>{title}</p>
         </div>
     );
+};
+
+GifGridIdem.propTypes = {
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
 };
