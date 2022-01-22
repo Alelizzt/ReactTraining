258d6a1d8a0905571775df8afe3bb714c8c14ed1
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-22 14:19:00 -0500

    LF as line ending

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { CounterApp } from "./components/01-useState/CounterApp";
+//import { CounterApp } from "./components/01-useState/CounterApp";
+import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
 // import { HookApp } from "./HookApp";
 
-ReactDOM.render(<CounterApp />, document.getElementById("root"));
+ReactDOM.render(<CounterWithCustomHook />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
