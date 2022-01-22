1d4719a5c4b761a1e2a9054f81b8730591a8a7f8
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-22 16:53:38 -0500

    useEffect con precauciones

--- 05-hook-app/src/components/02-useEffect/Message.js
+++ 05-hook-app/src/components/02-useEffect/Message.js
@@ -1,10 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const Message = () => {
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
+    const { x, y } = coords;
+
     useEffect(() => {
-        console.log("componente montado");
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+            setCoords(coords);
+        };
+        window.addEventListener("mousemove", mouseMove);
 
         return () => {
+            window.removeEventListener("mousemove", mouseMove);
             console.log("componente desmontado");
         };
     }, []);
@@ -12,6 +20,9 @@ export const Message = () => {
     return (
         <div>
             <h3>Eres la hostia tio!</h3>
+            <p>
+                x: {x} y:{y}
+            </p>
         </div>
     );
 };
