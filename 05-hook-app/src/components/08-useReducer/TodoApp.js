b2f00ec00c200039337988eed6fba555bbbaa50f
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-23 13:52:38 -0500

    Proyecto base TodoApp

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -16,13 +16,41 @@ export const TodoApp = () => {
     console.log(todos);
     return (
         <div>
-            <h1>Todo App</h1>
+            <h1>Todo App ({todos.length}) </h1>
             <hr />
-            <ol>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ol>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {todos.map((todo, i) => (
+                            <li key={todo.id} className="list-group-item">
+                                <p className="text-center">
+                                    {i + 1}. {todo.desc}
+                                </p>
+                                <button className="btn btn-danger">
+                                    Borrar
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            className=" form-control"
+                            placeholder="Aprender..."
+                            autoComplete="off"
+                        />
+                        <button className="btn btn-outline-primary mt-1 btn-block">
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
