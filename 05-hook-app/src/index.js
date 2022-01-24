43fad0ee1d694dcf94ac943e7203c8e161c92efb
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-24 17:13:27 -0500

    Utilizando React Router, useContext, Link y NavLink

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { TodoApp } from "./components/08-useReducer/TodoApp";
+import { MainApp } from "./components/09-useContext/MainApp";
+// import { TodoApp } from "./components/08-useReducer/TodoApp";
 // import { Padre } from "./components/07-tarea-memo/Padre";
 // import { CallbackHook } from "./components/06-memos/CallbackHook";
 // import { MemoHook } from "./components/06-memos/MemoHook";
@@ -15,7 +16,7 @@ import { TodoApp } from "./components/08-useReducer/TodoApp";
 // import SimpleForm from "./components/02-useEffect/SimpleForm";
 // import { HookApp } from "./HookApp";
 
-ReactDOM.render(<TodoApp />, document.getElementById("root"));
+ReactDOM.render(<MainApp />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
