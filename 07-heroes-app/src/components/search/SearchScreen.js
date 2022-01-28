daf5d7c5ba34a18d507534c221fa3e970cdbbd4f
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-01-27 21:05:56 -0500

    Search Component

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { useForm } from "../../hooks/useForm";
 
 export const SearchScreen = () => {
+    const [formValues, handleInputChange] = useForm({
+        searchText: "",
+    });
+
+    const { searchText } = formValues;
+    const handleSearch = (event) => {
+        event.preventDefault();
+    };
     return (
-        <div>
-            <h1>Search Screen</h1>
-        </div>
+        <>
+            <h1>Search</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-5">
+                    <h5>Search form</h5>
+                    <hr />
+
+                    <form onSubmit={handleSearch}>
+                        <input
+                            type="text"
+                            placeholder="Search a Hero"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            value={searchText}
+                            onChange={handleInputChange}
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-1"
+                        >
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     );
 };
