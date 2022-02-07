14d913b26675ab0ed1659f96c51eb357e2bd97eb
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-02-07 17:33:57 -0500

    Mostrando notas en el menu lateral y ajustando fechas con moment.js

--- 08-journal-app/src/components/journal/JournalEntry.js
+++ 08-journal-app/src/components/journal/JournalEntry.js
@@ -1,27 +1,28 @@
 import React from "react";
-
-export const JournalEntry = () => {
+import moment from "moment";
+export const JournalEntry = ({ id, date, title, body, url }) => {
+    const noteDate = moment(date);
     return (
         <div className="journal__entry pointer">
-            <div
-                className="journal__entry-picture"
-                style={{
-                    backgroundSize: "cover",
-                    backgroundImage:
-                        "url(https://millmanland.com/wp-content/uploads/2021/01/open-field.png)",
-                }}
-            ></div>
+            {url && (
+                <div
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundSize: "cover",
+                        backgroundImage: `url(${url})`,
+                    }}
+                ></div>
+            )}
 
             <div className="journal__entry-body">
-                <p className="journal__entry-title">A new day!</p>
-                <p className="journal__entry-content">
-                    Loren ipsum dolor de espalda.
-                </p>
+                <p className="journal__entry-title">{title}</p>
+                <p className="journal__entry-content">{body}</p>
             </div>
 
             <div className="journal__entry-date-box">
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{noteDate.format("dddd")}</span>
+
+                <h4>{noteDate.format("Do")}</h4>
             </div>
         </div>
     );
