ecd11145b264e1737ccf14a150c478bf9e3e9146
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-02-07 17:49:59 -0500

    Activando una nota para su edición

--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { NotesAppBar } from "./NotesAppBar";
+import { useSelector } from "react-redux";
+import { useForm } from "./../../hooks/useForm";
 
 export const NoteScreen = () => {
+    const { active: note } = useSelector((state) => state.notes);
+    const [formValues, handleInputChange] = useForm(note);
+
+    const { body, title } = formValues;
     return (
         <div className="notes__main-content">
             <NotesAppBar />
@@ -12,19 +18,25 @@ export const NoteScreen = () => {
                     placeholder="Some awesome title"
                     className="notes__title-input"
                     autoComplete="off"
+                    value={title}
+                    onChange={handleInputChange}
                 />
 
                 <textarea
                     placeholder="What happened today?"
                     className="notes__textarea"
+                    value={body}
+                    onChange={handleInputChange}
                 ></textarea>
 
-                <div className="notes__image">
-                    <img
-                        src="https://naigate.com/properties/wp-content/uploads/2020/12/Land-for-Sale-NaiGate.jpg"
-                        alt="land"
-                    />
-                </div>
+                {note.url && (
+                    <div className="notes__image">
+                        <img
+                            src="https://naigate.com/properties/wp-content/uploads/2020/12/Land-for-Sale-NaiGate.jpg"
+                            alt="land"
+                        />
+                    </div>
+                )}
             </div>
         </div>
     );
