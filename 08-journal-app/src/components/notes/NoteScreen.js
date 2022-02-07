c9ffcbd3f8a83597aee1b8efcd9d643db464a8e7
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-02-07 17:57:51 -0500

    Cambiando la nota activa al cambiar la selección

--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { NotesAppBar } from "./NotesAppBar";
 import { useSelector } from "react-redux";
 import { useForm } from "./../../hooks/useForm";
 
 export const NoteScreen = () => {
     const { active: note } = useSelector((state) => state.notes);
-    const [formValues, handleInputChange] = useForm(note);
+    const [formValues, handleInputChange, reset] = useForm(note);
 
     const { body, title } = formValues;
+
+    const activeId = useRef(note.id); //Guarda la referencia de la nota a re dibujar
+    useEffect(() => {
+        if (note.id !== activeId.current) {
+            reset(note);
+            activeId.current = note.id; //Actualiza la referencia
+        }
+    }, [note, reset]);
+
     return (
         <div className="notes__main-content">
             <NotesAppBar />
