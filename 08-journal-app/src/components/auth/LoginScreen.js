8c251cd58857fc4723cc5b3fd7ca1f7d47dd1d52
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-02-07 20:32:10 -0500

    Agregando animaciones a la aplicación

--- 08-journal-app/src/components/auth/LoginScreen.js
+++ 08-journal-app/src/components/auth/LoginScreen.js
@@ -26,7 +26,10 @@ export const LoginScreen = () => {
     return (
         <>
             <h3 className="auth__tittle">Login</h3>
-            <form onSubmit={handleLogin}>
+            <form
+                onSubmit={handleLogin}
+                className="animate__animated animate__fadeIn animate__faster"
+            >
                 <input
                     type="text"
                     placeholder="Email"
