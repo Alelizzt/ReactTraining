6f712049275448ccb63c44759121b56c2524b42e
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-02-07 20:25:20 -0500

    Purgando las notas al realizar logout

--- 08-journal-app/src/actions/notes.js
+++ 08-journal-app/src/actions/notes.js
@@ -16,12 +16,21 @@ export const startNewNote = () => {
 
         const doc = await db.collection(`${uid}/journal/notes`).add(newNote);
 
-        dispatch(activeNote(doc.ud, newNote));
+        dispatch(activeNote(doc.id, newNote));
+        dispatch(addNewNote(doc.id, newNote));
     };
 };
 
 export const activeNote = (id, note) => ({
     type: types.notesActive,
+    payload: {
+        id,
+        ...note,
+    },
+});
+
+export const addNewNote = (id, note) => ({
+    type: types.notesAddNew,
     payload: {
         id,
         ...note,
@@ -97,4 +106,8 @@ export const startDeleting = (id) => {
 export const deleteNote = (id) => ({
     type: types.notesDelete,
     payload: id,
+});
+
+export const noteLogout = () => ({
+    type: types.notesLogoutCleaning,
 });
