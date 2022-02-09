06b09e8974beb9eecdef8e056df021bc526c4823
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-02-09 16:47:04 -0500

    Pruebas en acciones sincronas - uiActions

--- 08-journal-app/src/actions/ui.js
+++ 08-journal-app/src/actions/ui.js
@@ -11,10 +11,8 @@ export const removeError = (err) => ({
 
 export const startLoading = () => ({
     type: types.uiStartLoading,
-    payload: true,
 });
 
 export const finishLoading = () => ({
     type: types.uiFinishLoading,
-    payload: false,
 });
---
+++ 08-journal-app/src/tests/actions/ui.test.js
@@ -0,0 +1,33 @@
+import { types } from "../../types/types";
+import {
+    finishLoading,
+    removeError,
+    setError,
+    startLoading,
+} from "./../../actions/ui";
+
+describe("Pruebas en ui-actions", () => {
+    test("Todas las acciones deben de funcionar", () => {
+        const action = setError("help!");
+
+        expect(action).toEqual({
+            type: types.uiSetError,
+            payload: "help!",
+        });
+        const removeErrorAction = removeError();
+        const startLoadingAction = startLoading();
+        const finishLoadingAction = finishLoading();
+
+        expect(removeErrorAction).toEqual({
+            type: types.uiRemoveError,
+        });
+
+        expect(startLoadingAction).toEqual({
+            type: types.uiStartLoading,
+        });
+
+        expect(finishLoadingAction).toEqual({
+            type: types.uiFinishLoading,
+        });
+    });
+});
