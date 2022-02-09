b544cada106fb8d916a74affe9ca748e875f980a
Ivan Lizzt <alejandrolistc@hotmail.com>
2022-02-09 16:10:40 -0500

    Utilizando el SDK de cloudinary para eliminar imagenes subidas en el test

--- 08-journal-app/src/tests/helpers/fileUpload.test.js
+++ 08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -1,4 +1,13 @@
+import "core-js";
+import cloudinary from "cloudinary";
 import { fileUpload } from "./../../helpers/fileUpload";
+
+cloudinary.config({
+    cloud_name: "dwswmmtt4",
+    api_key: "292412878912727",
+    api_secret: "LD4J8gH8ikxyWhFmPTbJP542Zes",
+    secure: true,
+});
 
 describe("Pruebas en fileUpload", () => {
     test("debe de cargar un archivo y retornar el URL", async () => {
@@ -11,6 +20,13 @@ describe("Pruebas en fileUpload", () => {
         const url = await fileUpload(file);
 
         expect(typeof url).toBe("string");
+        //Borrar imagen por ID
+        const segments = url.split("/");
+        const imageId = segments[segments.length - 1].replace(".png", "");
+
+        cloudinary.v2.api.delete_resources(imageId, {}, () => {
+            //done();
+        });
     });
 
     test("debe de retornar un error", async () => {
